8abcf6910489f62094305f671772acc5ca470b1f
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2013-07-12 10:19:16 +0900

    CategoryのScaffoldを作成

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MapRamble::Application.config.secret_key_base = '590878ae31daab4d7392b278f791d5c6eb44cdfad7c86c225e70492f0824343e1977486179efc4fbefc37dc697f47e589ee6f0d5952b9d09d5c293697c3a1b72'
